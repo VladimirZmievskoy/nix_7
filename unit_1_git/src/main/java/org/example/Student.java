37fc25d004739adfb077c05959b441646dee7920
Vladimir <vovazmievskoy2003@gmail.com>
2021-08-02 18:37:47 +0300

    second commit

--- unit_1_git/src/main/java/org/example/Student.java
+++ unit_1_git/src/main/java/org/example/Student.java
@@ -1,2 +1,20 @@
-package org.example;public class Student {
+package org.example;
+
+public class Student {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    @Override
+    public String toString() {
+        return "student{" +
+                "name ='"+ name + '\'' +
+                "}";
+    }
 }
+
--- unit_1_git/src/main/java/org/example/Unit1Main.java
+++ unit_1_git/src/main/java/org/example/Unit1Main.java
@@ -4,6 +4,9 @@ public class Unit1Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Unit1Main.main");
+      Student student = new Student();
+      student.setName("test");
+        System.out.println("student = " + student);
     }
+    
 }
