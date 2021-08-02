5a037a54ae319f8bb8d481fb9b39ca1d646a6a3f
Vladimir <vovazmievskoy2003@gmail.com>
2021-08-02 18:01:41 +0300

    add new class

--- unit_1_git/src/main/java/org/example/Unit1Main.java
+++ unit_1_git/src/main/java/org/example/Unit1Main.java
@@ -2,7 +2,8 @@ package org.example;
 
 public class Unit1Main
 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Unit1Main.main");
     }
 }
